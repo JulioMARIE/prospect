06f1651f44437b00f99b78dca0ad7b5bc3b20550
JulioMARIE <julioracechoisie@gmail.com>
2024-08-01 17:43:10 +0200

    bug fix

--- app/utils/withAuth.tsx
+++ app/utils/withAuth.tsx
@@ -1,16 +1,10 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 
-// Define the type for the wrapped component props
-type WrappedComponentProps = {
-  [key: string]: any;
-};
-
-const withAuth = (WrappedComponent: React.ComponentType<WrappedComponentProps>) => {
-  // Add display name for the HOC
-  const WithAuthWrapper: React.FC<WrappedComponentProps> = (props) => {
+const withAuth = (WrappedComponent: React.ComponentType) => {
+  return (props: any) => {
     const router = useRouter();
 
     useEffect(() => {
@@ -18,14 +12,10 @@ const withAuth = (WrappedComponent: React.ComponentType<WrappedComponentProps>) 
       if (!token) {
         router.replace('/login');
       }
-    }, [router]); // Include router in the dependency array
+    }, []);
 
     return <WrappedComponent {...props} />;
   };
-
-  WithAuthWrapper.displayName = `WithAuth(${WrappedComponent.displayName || WrappedComponent.name || 'Component'})`;
-
-  return WithAuthWrapper;
 };
 
 export default withAuth;
